d02923e4edce4937c89c57f2fd43f719c0c23057
Rony Aparicio <u6009311@ad.utah.edu>
2017-10-17 10:06:07 -0600

    server running

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/controller.js");
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
