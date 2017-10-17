fc5f1eb3a3ef48737c5975b4f454ea3cc14d7123
Rony Aparicio <u6009311@ad.utah.edu>
2017-10-17 10:33:54 -0600

    diplay index html

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/controller.js");
+const path = require('path');
+
+app.use(express.static(path.resolve('./public')));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
